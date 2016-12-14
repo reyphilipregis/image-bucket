c6e60f952ed40d4ae60462342dfc227099d277a7
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-14 10:56:26 +0800

    Chore: Create ImageDetail component

---
+++ client/components/image_detail.js
@@ -0,0 +1,14 @@
+// import react
+import React from 'react';
+
+// create component
+const ImageDetail = () => {
+	return (
+		<div>
+			Image Detail
+		</div>
+	);
+};
+
+// export component
+export default ImageDetail;
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,11 +1,28 @@
 // import react
-import React from 'react';
+import React 	   from 'react';
+import ImageDetail from './image_detail';
+
+// create dummy list of images
+const IMAGES = [
+	{
+		'title' : 'Pen',
+		'link'  : 'https://dummyimage.com/600x400'
+	},
+	{
+		'title' : 'Pine Tree',
+		'link'  : 'https://dummyimage.com/600x400'
+	},
+	{
+		'title' : 'Mug',
+		'link'  : 'https://dummyimage.com/600x400'
+	}
+];
 
 // create component
 const ImageList = () => {
 	return (
 		<ul>
-			<li>Image Detail</li>
+			<ImageDetail />
 		</ul>
 	);
 };
