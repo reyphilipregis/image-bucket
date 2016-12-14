c90ba51b9f94dcedcba7d1e83d969f31dafb8cd0
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-14 10:44:38 +0800

    Chore: Create ImageList component

---
+++ client/components/image_list.js
@@ -0,0 +1,14 @@
+// import react
+import React from 'react';
+
+// create component
+const ImageList = () => {
+	return (
+		<ul>
+			<li>Image Detail</li>
+		</ul>
+	);
+};
+
+// export component
+export default ImageList;
