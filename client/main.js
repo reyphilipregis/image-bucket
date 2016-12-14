16841cb828870e4cafccb2fac752912fb226028c
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-14 11:58:58 +0800

    Chore: Install Axios library to get AJAX request in imgur

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
+import Axios	 from 'axios'
 import ImageList from './components/image_list';
 
 // create component
@@ -11,6 +12,17 @@ const App = () => {
 	);
 };
 
+// run the anonymous function after meteor loaded all files
 Meteor.startup( () => {
+
+	// render the application component
 	ReactDOM.render( <App />, document.querySelector('.container') );
+
+	// temporary ajax request using axios
+	Axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+		.then((response) => { 
+					console.log(response) 
+		}
+	);
+
 } );
