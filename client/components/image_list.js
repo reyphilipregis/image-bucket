4237c7b2a631a91ef291527f944eb8d64008d73f
Rey Philip Regis <reyphilipregis@gmail.com>
2016-12-15 10:37:06 +0800

    Chore: Passing the dynamic data to Image list and displaying it

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -2,24 +2,9 @@
 import React, { Component } from 'react';
 import ImageDetail 			from './image_detail';
 
-// create dummy list of images
-const IMAGES = [
-	{
-		'title' : 'Pen',
-		'link'  : 'https://dummyimage.com/600x400'
-	},
-	{
-		'title' : 'Pine Tree',
-		'link'  : 'https://dummyimage.com/600x400'
-	},
-	{
-		'title' : 'Mug',
-		'link'  : 'https://dummyimage.com/600x400'
-	}
-];
-
-const ImageList = () => {
-	const RenderedImages = IMAGES.map((image) => {
+// create component
+const ImageList = (props) => {
+	const RenderedImages = props.images.map((image) => {
 		return <ImageDetail key={image.title} image={image} />
 	});
 
--- client/main.js
+++ client/main.js
@@ -26,7 +26,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<ImageList />
+				<ImageList images={this.state.images} />
 			</div>
 		);
 	}
